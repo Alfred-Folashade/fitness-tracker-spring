3424e822aeef546a1f5aa2baab0f973abc2a721e
Alfred-Folashade <alfredfolashade47@gmail.com>
2024-09-28 23:01:01 +0100

    Added attributes to Workout class and created WorkoutRepo class. I also created workout table in mysql

--- src/main/java/com/alfred/gym_tracker/Workout.java
+++ src/main/java/com/alfred/gym_tracker/Workout.java
@@ -2,7 +2,33 @@ package com.alfred.gym_tracker;
 
 import java.util.List;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Workout {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	private String muscleGroup;
+	
+	@OneToMany(cascade = CascadeType.ALL)
 	private List<Exercise> exercises;
+	
+	public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public List<Exercise> getExercises() {
+        return exercises;
+    }
+
+    public void setExercises(List<Exercise> exercises) {
+        this.exercises = exercises;
+    }
+
 }
