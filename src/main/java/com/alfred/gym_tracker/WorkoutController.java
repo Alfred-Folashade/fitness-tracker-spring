d5d84cf4cf48608446309744f6c06c77768c9b56
Alfred-Folashade <alfredfolashade47@gmail.com>
2024-10-06 21:19:22 +0100

    Updated workoutController class by adding CRUD methods which allow the user to create an manage data elements

--- src/main/java/com/alfred/gym_tracker/WorkoutController.java
+++ src/main/java/com/alfred/gym_tracker/WorkoutController.java
@@ -1,6 +1,14 @@
 package com.alfred.gym_tracker;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +16,33 @@ public class WorkoutController {
 	@Autowired
 	WorkoutRepo workoutRepo;
 	
+	@GetMapping(value="/workouts")
+	public List<Workout> getAllWorkouts(){
+		return workoutRepo.findAll();
+	}
 	
+	@GetMapping(value="/getWorkoutById{id}")
+	public Optional<Workout> getWorkoutById(@PathVariable Long id) {
+		return workoutRepo.findById(id);
+	}
+	
+	@DeleteMapping("/deleteWorkoutById{id}")
+	public void deleteById(@PathVariable Long id) {
+		Workout deleteWorkout = workoutRepo.findById(id).get();
+		workoutRepo.delete(deleteWorkout);
+	}
+	
+	
+	@PostMapping("/updateWorkoutById{id}")
+	public void updateWorkoutById(@PathVariable Long id, @RequestBody Workout workout) {
+		Workout updateWorkout = workoutRepo.findById(id).get();
+		updateWorkout.setExercises(workout.getExercises());
+		updateWorkout.setMuscleGroup(workout.getMuscleGroup());
+		workoutRepo.save(updateWorkout);
+	}
+	
+	@PostMapping("/saveWorkout")
+	public void saveUser(@RequestBody Workout newWorkout) {
+		workoutRepo.save(newWorkout);
+	}
 }
--- src/main/java/com/alfred/gym_tracker/WorkoutRepo.java
+++ src/main/java/com/alfred/gym_tracker/WorkoutRepo.java
@@ -3,5 +3,5 @@ package com.alfred.gym_tracker;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WorkoutRepo extends JpaRepository<Workout,Long>{
-
+	
 }
